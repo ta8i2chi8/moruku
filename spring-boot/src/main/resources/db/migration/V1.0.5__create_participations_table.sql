414ae0a6ad15896d447cb033c4c14c01942d3d87
ta8i2chi8 <moribook0828@gmail.com>
2023-09-02 12:11:13 +0900

    削除済みの練習会の参加情報が残ってしまい、getMyJoinedPracticesAPI実行時にエラーが発生してしまう事象を解決。
    解決方法：participationsテーブルのカラムに、外部キー制約を設定し、カスケードに削除されるように修正

--- spring-boot/src/main/resources/db/migration/V1.0.5__create_participations_table.sql
+++ spring-boot/src/main/resources/db/migration/V1.0.5__create_participations_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE participations (
     practice_id         binary(16),
     user_firebase_uid   varchar(100),
-    PRIMARY KEY (practice_id, user_firebase_uid)
+    PRIMARY KEY (practice_id, user_firebase_uid),
+    FOREIGN KEY (practice_id) REFERENCES practices(uuid) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (user_firebase_uid) REFERENCES users(firebase_uid) ON UPDATE CASCADE ON DELETE CASCADE
 );
